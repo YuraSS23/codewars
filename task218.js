4f67855678982520241048712861ce29b0a0dbb8
YuraSS23 <nikitsenko_yura@mail.ru>
2024-03-12 11:22:03 +0300

    task218

--- task218.js
+++ task218.js
@@ -15,8 +15,8 @@ Solution*/
 
 function isPrime(num) {
     if (num<2) return false
-    for (let i=2; i<100000; i++) {
-      if (num%i===0 && num!==i) return false
+    for (let i=2; i<=Math.sqrt(num); i++) {
+      if (num%i===0) return false
     }
     return true
   }
