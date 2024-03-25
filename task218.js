0c6f1aa81013f55fdf64029d46902c5de7979b97
YuraSS23 <nikitsenko_yura@mail.ru>
2024-03-25 11:19:07 +0300

    task218

--- task218.js
+++ task218.js
@@ -7,9 +7,9 @@ You can assume you will be given an integer input.
 You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
 NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
 Example
-is_prime(1) false 
-is_prime(2) true 
-is_prime(-1) false
+is_prime(1)  false 
+is_prime(2)  true 
+is_prime(-1)  false
 
 Solution*/
 
